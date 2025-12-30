e4b7cd58177a28150cbffee446fb08e3058b1669
Yehonatan Battat <yehonatan.battat@zeronetworks.com>
2025-12-30 08:44:10 +0000

    feat(tc extensions): add support for tc chains

--- pyroute2/netlink/rtnl/tcmsg/__init__.py
+++ pyroute2/netlink/rtnl/tcmsg/__init__.py
@@ -66,7 +66,7 @@ class tcmsg(nlmsg):
         ('parent', 'I'),
         ('info', 'I'),
     )
-
+    # Extended from /include/uapi/linux/rtnetlink.h and /net/sched/sch_api.c
     nla_map = (
         ('TCA_UNSPEC', 'none'),
         ('TCA_KIND', 'asciiz'),
@@ -77,6 +77,9 @@ class tcmsg(nlmsg):
         ('TCA_FCNT', 'hex'),
         ('TCA_STATS2', 'get_stats2'),
         ('TCA_STAB', 'hex'),
+        ('TCA_PAD', 'uint32'),
+        ('TCA_DUMP_INVISIBLE', 'none'),
+        ('TCA_CHAIN', 'uint32')
     )
 
     class stats(nla):
