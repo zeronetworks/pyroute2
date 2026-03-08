9fda3e643b47ebdbb415f3c6cf211f4ed1633090
Yehonatan Battat <yehonatan.battat@zeronetworks.com>
2026-03-08 10:43:11 +0000

    fix(common ematch): missing convertion to bytes in py27

--- pyroute2/netlink/rtnl/tcmsg/common_ematch.py
+++ pyroute2/netlink/rtnl/tcmsg/common_ematch.py
@@ -93,7 +93,7 @@ def get_tcf_ematches(kwarg):
         data.encode()
 
         # Add ematch encoded data
-        match['opt'] = data.data
+        match['opt'] = bytes(data.data)
 
         # Safety check
         if i == expr_count - 1 and 'relation' in cur_match:
